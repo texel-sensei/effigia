963f40cbb1d70bc643f4e2921545ef0bdd66101b
texel <texelsensei@posteo.net>
2021-04-06 22:35:27 +0200

    feat: implement proof of concept image loading
    Loads an image and displays it via SDL in a fixed resolution.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 #include <dlfcn.h>
 #include <memory>
 #include <iostream>
@@ -17,6 +20,9 @@ void noop(void*) {}
 class Plugin {
 	using initialize_fp = void*(*)();
 	using destroy_fp = void(*)(void*);
+	using clear_fp = int(*)(void*);
+	using set_pixel_fp = int(*)(void*, int, int, int);
+	using display_fp = int(*)(void*);
 
 	using display_ptr = std::unique_ptr<void, destroy_fp>;
 public:
@@ -28,15 +34,37 @@ public:
 		}
 		initialize_display = load_function<initialize_fp>("initialize_display");
 		destroy_display = load_function<destroy_fp>("destroy_display");
+		clear_display = load_function<clear_fp>("clear");
+		set_pixel_display = load_function<set_pixel_fp>("set_pixel");
+		display_display = load_function<display_fp>("display");
 
 		auto* d = initialize_display();
+		if(!d) {
+			throw runtime_error{"Failed to initialize display!"};
+		}
 		display = {d, destroy_display};
+	}
+
+	void clear() {
+		clear_display(display.get());
+	}
+
+	void set_pixel(int x, int y, int color) {
+		set_pixel_display(display.get(), x, y, color);
+	}
+
+	void display_() {
+		display_display(display.get());
 	}
 
 private:
 	dl_ptr plugin_library;
 	initialize_fp initialize_display = {};
 	destroy_fp destroy_display = {};
+	clear_fp clear_display = {};
+	set_pixel_fp set_pixel_display = {};
+	display_fp display_display = {};
+
 	display_ptr display = {nullptr, noop};
 
 	template<typename F>
@@ -51,6 +79,34 @@ private:
 };
 
 int main(int argc, char* argv[]) {
+	if(argc != 2) {
+		return 1;
+	}
 	Plugin sdl_plugin{"plugins/libsdl_display.so"};
 	std::cout << "successfully loaded plugin" << std::endl;
+
+	auto* file = argv[1];
+	std::cout << "Loading image " << file << std::endl;
+	int x, y, n;
+	unsigned char* data = stbi_load(file, &x, &y, &n, 4);
+	if(!data) {
+		std::cout << "Can't load image!" << std::endl;
+		return 1;
+	}
+	sdl_plugin.clear();
+	for(int line = 0; line < y; ++line) {
+		for(int col = 0; col < x; ++col) {
+			auto pixel = (line * x + col)*4;
+			uint8_t rgb[3];
+			for(int channel = 0; channel < 3; ++channel) {
+				rgb[channel] = data[pixel + channel];
+			}
+			auto color = (rgb[0] << 16) | (rgb[1] << 8) | rgb[2];
+			sdl_plugin.set_pixel(col, line, color);
+		}
+	}
+	stbi_image_free(data);
+	sdl_plugin.display_();
+	int c;
+	std::cin >> c;
 }
--- src/sdl_display/plugin_main.cpp
+++ src/sdl_display/plugin_main.cpp
@@ -14,21 +14,40 @@ public:
 		if(SDL_Init(SDL_INIT_VIDEO) != 0) {
 			throw runtime_error{"Failed to init SDL!"};
 		}
-		window = SDL_CreateWindow("", 100, 100, 600, 800, 0);
+		window = SDL_CreateWindow("", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 800, SDL_WINDOW_SHOWN);
 		if(!window) {
 			throw runtime_error{"Failed to create window!"};
 		}
-		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE | SDL_RENDERER_PRESENTVSYNC);
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+		surface = SDL_CreateRGBSurface(0, 750, 1000, 32, 0x00FF0000, 0x0000FF00, 0x000000FF,0xFF000000);
+		if(!surface) {
+			cerr << "Failed to create surface!" << endl;
+//			throw runtime_error{"Failed to create surface!"};
+		}
 	}
 	~Display() {
+		SDL_FreeSurface(surface);
 		SDL_DestroyRenderer(renderer);
 		SDL_DestroyWindow(window);
 		SDL_Quit();
 	}
 
-private:
+	void clear() {
+		SDL_RenderClear(renderer);
+	}
+
+	void present() {
+		auto texture = SDL_CreateTextureFromSurface(renderer, surface);
+		SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_NONE);
+		SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+		SDL_RenderPresent(renderer);
+		SDL_DestroyTexture(texture);
+	}
+
+//private:
 	SDL_Window* window;
 	SDL_Renderer* renderer;
+	SDL_Surface* surface;
 };
 
 #ifdef __cplusplus
@@ -59,12 +78,20 @@ extern "C" {
 	}
 
 	int clear(void* display) {
-		return -1;
+		auto d = static_cast<Display*>(display);
+		d->clear();
+		return 0;
 	}
 	int set_pixel(void* display, int x, int y, int color) {
+		auto d = static_cast<Display*>(display);
+
+		static_cast<uint32_t*>(d->surface->pixels)[y*d->surface->w+x] = color;
+
 		return -1;
 	}
 	int display(void* display) {
+		auto d = static_cast<Display*>(display);
+		d->present();
 		return -1;
 	}
 
