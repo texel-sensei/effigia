f9b311947278069cf778f0441c530e219d717172
texel <texelsensei@posteo.net>
2021-04-28 15:44:45 +0200

    fix: fix waveshare compile issues
    Code didn't actually compile.

--- src/waveshare_7in5/main.c
+++ src/waveshare_7in5/main.c
@@ -20,6 +20,8 @@ typedef enum ColorName {
 	black = 0, white, red
 } ColorName;
 
+void Handler(int signo);
+
 static WaveshareDisplay* alloc_display() {
 	WaveshareDisplay* display = malloc(sizeof(WaveshareDisplay));
 	if (!display) return NULL;
@@ -58,6 +60,9 @@ void* initialize_display() {
 
     Paint_NewImage(display->red, EPD_WIDTH, EPD_HEIGHT, 0, WHITE);
     Paint_NewImage(display->black, EPD_WIDTH, EPD_HEIGHT, 0, WHITE);
+
+    // Exception handling:ctrl + c
+    signal(SIGINT, Handler);
 
 	return display;
 }
@@ -129,6 +134,14 @@ int present(void* user_data) {
 	return 0;
 }
 
+DisplayEvent* poll_events(void* display){
+	static DisplayEvent always_quit;
+	always_quit.id = 0;
+	return &always_quit;
+}
+
+void free_event(DisplayEvent* event){}
+
 void Handler(int signo)
 {
     //System Exit
@@ -137,89 +150,4 @@ void Handler(int signo)
     DEV_ModuleExit();
 
     exit(0);
-}
-
-int main(int argc, char* argv[])
-{
-    // Exception handling:ctrl + c
-    signal(SIGINT, Handler);
-
-    if(EPD_Init() != 0) {
-        printf("e-Paper init failed\r\n");
-    }
-    printf("clear...\r\n");
-
-    // Create a new image cache named IMAGE_BW and fill it with white
-    UBYTE *BlackImage, *RedImage;
-    UWORD Imagesize = ((EPD_WIDTH % 8 == 0)? (EPD_WIDTH / 8 ): (EPD_WIDTH / 8 + 1)) * EPD_HEIGHT;
-    if((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
-        printf("Failed to apply for black memory...\r\n");
-        exit(0);
-    }
-    if((RedImage = (UBYTE *)malloc(Imagesize)) == NULL) {
-        printf("Failed to apply for red memory...\r\n");
-        exit(0);
-    }
-    printf("NewImage:BlackImage and RedImage\r\n");
-    Paint_NewImage(BlackImage, EPD_WIDTH, EPD_HEIGHT, 0, WHITE);
-    Paint_NewImage(RedImage, EPD_WIDTH, EPD_HEIGHT, 0, WHITE);
-
-    //Select Image
-    Paint_SelectImage(BlackImage);
-    Paint_Clear(WHITE);
-    Paint_SelectImage(RedImage);
-    Paint_Clear(WHITE);
-
-	FILE* source_folder_file = fopen(GALLERY_FILE, "r");
-	if(source_folder_file == NULL){
-		printf("Failed to open gallery file %s\n", GALLERY_FILE);
-		exit(1);
-	}
-
-	char* folder = NULL;
-	size_t len = 0;
-	// Read full file
-	ssize_t bytes_read = getline(&folder, &len, source_folder_file);
-	if(bytes_read == -1) {
-		printf("Failed to read gallery file %s\n", GALLERY_FILE);
-		exit(1);
-	}
-	len = bytes_read;
-	fclose(source_folder_file);
-
-	if (folder[len-1] == '\n')
-		folder[len-1] = '\0'; // remove newline in foldername
-
-	const char* format = "%s/%s-%c.bmp";
-	char* name = argv[1];
-	int size = snprintf(NULL, 0, format, folder, name, 'r') + 1;
-	char* path = malloc(size);
-
-    printf("show bmp------------------------\r\n");
-    printf("read black bmp\r\n");
-    Paint_SelectImage(BlackImage);
-    Paint_Clear(WHITE);
-	snprintf(path, size, format, folder, name, 'b');
-	printf("reading image file %s\n", path);
-    GUI_ReadBmp(path, 0, 0);
-
-    printf("read red bmp\r\n");
-    Paint_SelectImage(RedImage);
-    Paint_Clear(WHITE);
-	snprintf(path, size, format, folder, name, 'r');
-    GUI_ReadBmp(path, 0, 0);
-
-	free(path);
-	free(folder);
-
-    EPD_Display(BlackImage, RedImage);
-
-    printf("Goto Sleep mode...\r\n");
-    EPD_Sleep();
-    free(BlackImage);
-    BlackImage = NULL;
-    free(RedImage);
-    RedImage = NULL;
-
-    return 0;
 }
