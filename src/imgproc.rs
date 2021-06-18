18d993c0885291ff065787e823f39df60b2bbafd
texel <texelsensei@posteo.net>
2021-06-18 20:02:20 +0200

    refactor: move image processing into own module
    Cleaned up the code a bit and separated the processing stuff into a new
    file.

---
+++ src/imgproc.rs
@@ -0,0 +1,149 @@
+use crate::{DisplayProperties};
+use image;
+use image::{Pixel};
+
+pub type ProcessedImage = Vec<u32>;
+
+#[derive(Debug)]
+pub struct ColorVec(Vec<image::Rgb<u8>>);
+
+
+pub struct ImageProcessor {
+    target_format: DisplayProperties,
+    palette: Option<ColorVec>,
+}
+
+
+// Note: Code taken from the image crate.
+/// Calculates the width and height an image should be resized to.
+/// This preserves aspect ratio, and based on the `fill` parameter
+/// will either fill the dimensions to fit inside the smaller constraint
+/// (will overflow the specified bounds on one axis to preserve
+/// aspect ratio), or will shrink so that both dimensions are
+/// completely contained with in the given `width` and `height`,
+/// with empty space on one axis.
+fn resize_dimensions(width: u32, height: u32, nwidth: u32, nheight: u32, fill: bool) -> (u32, u32) {
+    let ratio = u64::from(width) * u64::from(nheight);
+    let nratio = u64::from(nwidth) * u64::from(height);
+
+    let use_width = if fill {
+        nratio > ratio
+    } else {
+        nratio <= ratio
+    };
+    let intermediate = if use_width {
+        u64::from(height) * u64::from(nwidth) / u64::from(width)
+    } else {
+        u64::from(width) * u64::from(nheight) / u64::from(height)
+    };
+    let intermediate = std::cmp::max(1, intermediate);
+    if use_width {
+        if intermediate <= u64::from(::std::u32::MAX) {
+            (nwidth, intermediate as u32)
+        } else {
+            (
+                (u64::from(nwidth) * u64::from(::std::u32::MAX) / intermediate) as u32,
+                ::std::u32::MAX,
+            )
+        }
+    } else if intermediate <= u64::from(::std::u32::MAX) {
+        (intermediate as u32, nheight)
+    } else {
+        (
+            ::std::u32::MAX,
+            (u64::from(nheight) * u64::from(::std::u32::MAX) / intermediate) as u32,
+        )
+    }
+}
+
+impl ImageProcessor {
+    pub fn new(target_format: DisplayProperties, palette: Option<ColorVec>) -> Self {
+        Self{ target_format, palette }
+    }
+
+
+    pub fn process<Image>(&self, input: &Image) -> ProcessedImage
+        where Image: image::GenericImageView<Pixel=image::Rgb<u8>>
+    {
+        let mut output = Vec::new();
+        let pixel_count = self.target_format.width * self.target_format.height;
+
+        output.resize(pixel_count as usize, u32::default());
+
+        let (w,h) = resize_dimensions(
+            input.width(), input.height(),
+            self.target_format.width as u32, self.target_format.height as u32,
+            false
+        );
+        println!("Resized {:?} to {:?}", (self.target_format.width, self.target_format.height), (w,h));
+
+        let mut input = image::imageops::resize(
+            input,
+            w,h,
+            image::imageops::FilterType::CatmullRom
+        );
+
+        let x_off = (self.target_format.width as u32 - input.dimensions().0)/2;
+        let y_off = (self.target_format.height as u32 - input.dimensions().1)/2;
+        let index = |x,y| {(y * (self.target_format.width as u32)+x) as usize};
+
+        if let Some(palette) = self.palette.as_ref() {
+            image::imageops::dither(&mut input, palette);
+            use image::imageops::colorops::ColorMap;
+            for (x, y, pixel) in input.enumerate_pixels() {
+                let color = palette.index_of(&pixel) as u32;
+                output[index(x+x_off, y+y_off)] = color;
+            }
+        } else {
+            for (x, y, pixel) in input.enumerate_pixels() {
+                let color = conv(&pixel);
+                output[index(x+x_off, y+y_off)] = color;
+            }
+        };
+
+        output
+    }
+
+    pub fn index_to_pos(&self, index: usize) -> (u32, u32) {
+        (
+            index as u32 % self.target_format.width as u32,
+            index as u32 / self.target_format.width as u32
+        )
+    }
+}
+
+fn conv(color: &image::Rgb<u8>) -> u32 {
+    color[2] as u32 | ((color[1] as u32) << 8) | ((color[0] as u32) << 16)
+}
+
+// TODO: this isn't a very good distance function as it compares in RGB space
+fn color_distance(a: image::Rgb<u8>, b: image::Rgb<u8>) -> i32 {
+    a.0.iter().zip(b.0.iter()).map(|(a,b)| ((*a as i32 - *b as i32)).pow(2)).sum::<i32>()
+}
+
+impl image::imageops::colorops::ColorMap for ColorVec {
+    type Color=image::Rgb<u8>;
+
+    fn index_of(&self, color: &Self::Color) -> usize {
+        (0..self.0.len()).min_by_key(|idx| color_distance(self.0[*idx], *color)).expect("Can't have empty color map!")
+    }
+
+    fn map_color(&self, color: &mut Self::Color) {
+        *color = self.0[self.index_of(color)];
+    }
+}
+
+fn u32_to_rgb(color: u32) -> image::Rgb<u8> {
+    image::Rgb::<u8>::from_channels(
+        ((color & 0xFF0000) >> 16) as u8,
+        ((color & 0x00FF00) >>  8) as u8,
+        ((color & 0x0000FF) >>  0) as u8,
+        0
+    )
+}
+
+impl From<Vec<u32>> for ColorVec {
+    fn from(vec: Vec<u32>) -> Self {
+        ColorVec{0: vec.iter().map(|c| u32_to_rgb(*c)).collect()}
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,24 @@
 use std::ffi::CStr;
 
 extern crate image;
-use image::Pixel;
 
 mod display;
 use crate::display::*;
 
-#[derive(Debug)]
-struct ColorVec(Vec<image::Rgb<u8>>);
+mod imgproc;
+use imgproc::{ImageProcessor};
 
-// TODO: this isn't a very good distance function as it compares in RGB space
-fn color_distance(a: image::Rgb<u8>, b: image::Rgb<u8>) -> i32 {
-    a.0.iter().zip(b.0.iter()).map(|(a,b)| ((*a as i32 - *b as i32)).pow(2)).sum::<i32>()
-}
-
-impl image::imageops::colorops::ColorMap for ColorVec {
-    type Color=image::Rgb<u8>;
-
-    fn index_of(&self, color: &Self::Color) -> usize {
-        (0..self.0.len()).min_by_key(|idx| color_distance(self.0[*idx], *color)).expect("Can't have empty color map!")
-    }
-
-    fn map_color(&self, color: &mut Self::Color) {
-        *color = self.0[self.index_of(color)];
-    }
-}
-
-fn u32_to_rgb(color: u32) -> image::Rgb<u8> {
-    image::Rgb::<u8>::from_channels(
-        ((color & 0xFF0000) >> 16) as u8,
-        ((color & 0x00FF00) >>  8) as u8,
-        ((color & 0x0000FF) >>  0) as u8,
-        0
-    )
-}
-
-impl From<Vec<u32>> for ColorVec {
-    fn from(vec: Vec<u32>) -> Self {
-        ColorVec{0: vec.iter().map(|c| u32_to_rgb(*c)).collect()}
-    }
-}
-
-fn conv(color: &image::Rgb<u8>) -> u32 {
-    color[2] as u32 | ((color[1] as u32) << 8) | ((color[0] as u32) << 16)
-}
-
-fn display_image_file(display: &mut Display, file: &str) -> Result<(), i32> {
-    let properties = display.query_display_properties().expect("Failed to query properties");
-
+fn display_image_file(display: &mut Display, processor: &ImageProcessor, file: &str) -> Result<(), i32> {
     println!("Loading image {}", file);
-    let mut img = image::open(file)
-        .expect("Can't load image!")
-        .resize(properties.width as u32, properties.height as u32, image::imageops::FilterType::CatmullRom)
-        .into_rgb8();
+    let img = image::open(file).expect("Can't load image!").into_rgb8();
 
-    let palette: Option<ColorVec>;
-    if properties.mode == ColorMode_indexed {
-        palette = Some(display.query_color_palette().unwrap().into());
-        image::imageops::dither(&mut img, palette.as_ref().unwrap());
-    } else {palette = None;}
+    let processed = processor.process(&img);
 
     display.clear()?;
 
-    let x_off = (properties.width as u32 - img.dimensions().0)/2;
-    let y_off = (properties.height as u32 - img.dimensions().1)/2;
-
-    for (x, y, pixel) in img.enumerate_pixels() {
-        let color = if let Some(pal) = palette.as_ref() {
-            use image::imageops::colorops::ColorMap;
-            pal.index_of(pixel) as u32
-        } else {
-            conv(pixel)
-        };
-        display.set_pixel(x+x_off, y+y_off, color)?;
+    for i in 0..processed.len() {
+        let (x,y) = processor.index_to_pos(i);
+        display.set_pixel(x, y, processed[i])?;
     }
 
     display.present()?;
@@ -88,7 +34,13 @@ fn main() {
         let args: Vec<String> = std::env::args().collect();
         let file = &args[1];
 
-        let result = display_image_file(&mut display, file);
+        let props = display.query_display_properties().expect("Failed to get display properies!");
+        let palette = if props.mode == ColorMode_indexed {
+            Some(display.query_color_palette().expect("Failed to query color palette!").into())
+        } else { None };
+        let processor = imgproc::ImageProcessor::new(props, palette);
+
+        let result = display_image_file(&mut display, &processor, file);
         if let Err(code) = result {
             println!("Failed to display image! Code {}", code);
         }
@@ -101,7 +53,7 @@ fn main() {
                     EFFIGIA_EVENT_SHUTDOWN_REQUESTED => done = true,
                     EFFIGIA_EVENT_NEW_IMAGE_REQUESTED => {
                         let file = unsafe {CStr::from_ptr(event.data.new_image.filename) };
-                        let result = display_image_file(&mut display, file.to_str().unwrap());
+                        let result = display_image_file(&mut display, &processor, file.to_str().unwrap());
                         if let Err(code) = result {
                             println!("Failed to display image! Code {}", code);
                         }
